73b922c47fe4242c5d77645cc66166ad4088d2e1
Fidelis Wandia <fidelismundia2000@gmail.com>
2022-06-14 15:33:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const bulb = document.getElementById("bulb")
+console.log(bulb)
+
+bulb.addEventListener("click", function(e){
+    e.target.classList.toggle("light-on")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
